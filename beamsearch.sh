be2df494fccf0a88798658b732a0a43021d96977
Omkar Ingale <51068625+IngaleOmkar@users.noreply.github.com>
2025-11-03 16:18:48 +0100

    updated shell script

--- beamsearch.sh
+++ beamsearch.sh
@@ -12,22 +12,6 @@ module load mamba
 source activate atmt
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
 
-
-# PREPARE DATA
-python preprocess.py \
-    --source-lang cz \
-    --target-lang en \
-    --raw-data ~/shares/cz-en/data/raw \
-    --dest-dir ./cz-en/data/prepared \
-    --model-dir ./cz-en/tokenizers \
-    --test-prefix test \
-    --train-prefix train \
-    --valid-prefix valid \
-    --src-vocab-size 8000 \
-    --tgt-vocab-size 8000 \
-    --src-model ./cz-en/tokenizers/cz-bpe-8000.model \
-    --tgt-model ./cz-en/tokenizers/en-bpe-8000.model
-
 # TRANSLATE
 python translate.py \
     --cuda \

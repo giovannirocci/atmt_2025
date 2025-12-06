6aa28f87705665b30e2ab6b116a23b9fb6255032
Omkar Ingale <51068625+IngaleOmkar@users.noreply.github.com>
2025-12-06 21:31:22 +0100

    working a5q4

---
+++ assignment5/greedysearch.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash -l
+#SBATCH --partition teaching
+#SBATCH --time=24:0:0
+#SBATCH --ntasks=1
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+#SBATCH --output=out_a3_p2_greedy.out
+
+module load gpu
+module load mamba
+source activate atmt
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
+
+# TRANSLATE
+python translate.py \
+    --cuda \
+    --input ~/data/a3/atmt_2025/Assignment\ 3/toy_example/data/raw/test.cz \
+    --src-tokenizer ~/data/atmt_2025/cz-en/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer ~/data/atmt_2025/cz-en/tokenizers/en-bpe-8000.model \
+    --checkpoint-path ~/data/atmt_2025/cz-en/checkpoints/checkpoint_best.pt \
+    --output ~/data/a3/atmt_2025/Assignment\ 3/output-a3-greedy.txt \
+    --max-len 300 \
+    --bleu \
+    --reference ~/data/a3/atmt_2025/Assignment\ 3/toy_example/data/raw/test.en \
